605996be41ab694c28a202def5e2ce726c966ea9
ahbabislam <ahbabislam1995@gmail.com>
2019-09-02 14:25:36 +0600

    My first express server and routes

--- server.js
+++ server.js
@@ -2,10 +2,39 @@ const express = require('express')
 
 const app = express();
 
-const PORT = process.env.PORT || 4000 ;
+
+
+app.get('/about', (req, res) => {
+    let result = res.send('<h1>Hi I am About Page</h1>')
+    console.log(result)
+})
+
+
+app.get('/blog', (req, res) => {
+    let result = res.send('<h1>Hi I am Blog Page</h1>')
+    console.log(result)
+})
+
+
+
+
+app.get('/', (req, res) => {
+    let result = res.end('<h1>Hi my name is Ahbab</h1>')
+    console.log(result)
+})
+
+
+app.get('*', (req, res) => {
+    let result = res.end('<h1>404 Not Found...!</h1>')
+    console.log(result)
+})
+
+
+
+// Database connection
+ 
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is Running on Port ${PORT}`)
 })
-
-
